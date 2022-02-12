bd44e37aeef6c814cc5839e11d7cd43622b26915
jubaer <jubaerhosain1119@gmail.com>
2022-02-12 17:30:46 +0600

    Created and Updated some input file in bangla.stemmer and created a method at Stemmer class

--- src/bangla/stemmer/Stemmer.java
+++ src/bangla/stemmer/Stemmer.java
@@ -4,12 +4,65 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Stemmer {
-    private TreeSet<String> notSteamed;
-    private TreeSet<String> bochonSuffix;
-    private TreeSet<String> bivoktiSuffix;
+    private TreeSet<String> notStem;
+    private TreeSet<String> bochonSuffixes;
+    private TreeSet<String> bivoktiSuffixes;
+    private TreeSet<String> extraSuffixes;
+    private TreeMap<String, String> replaceSuffixes;
+    private TreeSet<String> otherSuffixes;
+
 
     public Stemmer() {
-        
+        notStem = new TreeSet<>();
+        bochonSuffixes = new TreeSet<>();
+        bivoktiSuffixes = new TreeSet<>();
+        extraSuffixes = new TreeSet<>();
+        replaceSuffixes = new TreeMap<>();
+        otherSuffixes = new TreeSet<>();
+        readNotStem();
+        readBochonSuffixes();
+        readBivoktiSuffixes();
+        readExtraSuffixes();
+        readReplaceSuffixes();
+        readOtherSuffixes();
+    }
+
+    private void readOtherSuffixes() {
+    }
+
+    private void readReplaceSuffixes() {
+    }
+
+    private void readExtraSuffixes() {
+    }
+
+    private void readBivoktiSuffixes() {
+    }
+
+    private void readBochonSuffixes() {
+    }
+
+    private void readNotStem() {
+    }
+
+    private boolean suffixMatcher(String suffix, String word) {
+        int m = suffix.length()-1;
+        int n = word.length()-1;
+
+        if(m > n) {
+            return false;
+        }
+
+        while(m >= 0 && n >= 0) {
+            if(suffix.charAt(m) == word.charAt(n) || suffix.charAt(m) == '.') {
+                m--;
+                n--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public String findStem(String word) {
@@ -17,9 +70,9 @@ public class Stemmer {
 
         for(int i = word.length()-1; i >= 0; i++) {
             String suffix = word.substring(i);
-            if(notSteamed.contains(suffix)) {
+            if(notStem.contains(suffix)) {
                 return word;
-            } else if(bochonSuffix.contains(suffix)) {
+            } else if(bochonSuffixes.contains(suffix)) {
                 word = word.substring(0, i);
                 return word;
             }
