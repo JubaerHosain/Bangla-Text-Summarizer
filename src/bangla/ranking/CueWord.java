b08bf528dbe1fd9ab8c44446ab1d6cf2f9dadede
jubaer <jubaerhosain1119@gmail.com>
2022-02-15 14:51:19 +0600

    Updated CueWord.java

--- src/bangla/ranking/CueWord.java
+++ src/bangla/ranking/CueWord.java
@@ -1,14 +1,42 @@
 package bangla.ranking;
 
 import my.library.Trie;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 
 /**This class implements all necessary method 
  * required for cue word processing*/
 public class CueWord {
-    Trie cueWords;
+    private Trie cueWords;
     
-    public CueWord() {
+    public CueWord() throws IOException {
         cueWords = new Trie();
+        readCueWords(cueWords, "cue_words.txt");
     }
-    
+
+    private void readCueWords(Trie trie, String fileName) throws IOException {
+        URL url = this.getClass().getResource(fileName);
+        File file = new File(url.getFile());
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        while (bufferedReader.ready()) {
+            String line = bufferedReader.readLine();
+            line = line.trim();
+            if(line.length() < 1) continue;
+            System.out.println(line);
+            trie.add(line);
+        }
+        bufferedReader.close();
+    }
+
+    public boolean contains(String string) {
+        return cueWords.contains(string);
+    }
+
+    public static void main(String[] args) throws IOException {
+        CueWord cueWord = new CueWord();
+        cueWord.contains("");
+    }
 }
