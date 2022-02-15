f9fc3518a8d5f34153721d713db85a80e527ceb4
jubaer <jubaerhosain1119@gmail.com>
2022-02-15 11:46:56 +0600

    Created class CueWord.java

---
+++ src/bangla/sentence_ranking/CueWord.java
@@ -0,0 +1,14 @@
+package bangla.sentence_ranking;
+
+import my.library.Trie;
+
+/**This class implements all necessary method 
+ * required for cue word processing*/
+public class CueWord {
+    Trie cueWords;
+    
+    public CueWord() {
+        cueWords = new Trie();
+    }
+    
+}
